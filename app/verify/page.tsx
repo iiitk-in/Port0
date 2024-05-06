68c15e265e4dc8872361d55c6136a9e4b75086bf
HarshIyer <harshessi@gmail.com>
2024-05-06 21:39:17 +0530

    Add register params

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -3,15 +3,29 @@ import react from "react";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import crypto from 'crypto';
+
+var pbkdf2 = require('pbkdf2')
 
 const Verify = () => {
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    //will do this later
     //post to /auth/verify with the OTP and the email
     //response will be a jwt token
     //then perform the hashing for /auth/create
+    //then redirect to /create
+
   };
+  const [salt, setSalt] = useState(''); 
+  setSalt(crypto.randomBytes(16).toString('base64'));
+  const [token,setToken] = useState(''); // get this from localstorage maybe
+  const [email,setEmail] = useState(''); 
+  const [password,setPassword] = useState('');
+
+  var derivedKey = pbkdf2.pbkdf2Sync(password, salt, 1000, 32, 'sha256');
+  
+
   return (
     <main className="flex md:flex-row flex-col text-center bg-gradient-to-b from-amber-100 to-white dark:from-[#020024] dark:to-[#020024]">
       <div>
