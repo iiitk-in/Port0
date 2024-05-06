28333ce76c0db5c573dc816962e29405f349a62f
HarshIyer <harshessi@gmail.com>
2024-05-07 01:01:59 +0530

    Add space

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,23 +1,22 @@
 //INCOMPLETE
 //This page will be shown after OTP is verified and account is created
-import {useState} from "react";
-const Page = () =>{
-    const [auth,setAuth] = useState(false);
-    //maybe get auth status from status code returned from the post made after OTP
-    if(auth){
-        return (
-            <div>
-                <h1>Authenticated! and account created</h1>
-                {/* redirect to account homepage */}
-            </div>
-        )
-    }
-    else{
-        return (
-            <div>
-                <h1>Problem in creating account! Try again</h1>
-                {/* redirect to /register */}
-            </div>
-        )
-    }
-}
+import { useState } from "react";
+const Page = () => {
+  const [auth, setAuth] = useState(false);
+  //maybe get auth status from status code returned from the post made after OTP
+  if (auth) {
+    return (
+      <div>
+        <h1>Authenticated! and account created</h1>
+        {/* redirect to account homepage */}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Problem in creating account! Try again</h1>
+        {/* redirect to /register */}
+      </div>
+    );
+  }
+};
