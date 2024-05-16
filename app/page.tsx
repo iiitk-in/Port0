d6bf3446302ce18f5d2b33b55fbd56f1d1c1e908
HarshIyer <harshessi@gmail.com>
2024-05-17 01:47:46 +0530

    Fix overflow on home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,36 +10,41 @@ export default function Page() {
   const dark = useSelector((state: RootState) => state.darkMode);
 
   return (
-    <main
+    <section
       className={
         dark
-          ? "overflow-auto flex-col bg-gradient-to-b from-[#020024] to-[#020024] sm:overflow-x-hidden"
-          : "overflow-auto flex-col bg-gradient-to-b from-amber-100 to-white sm:overflow-x-hidden"
+          ? "overflow-auto h-screen flex-col bg-gradient-to-b from-[#020024] to-[#020024] sm:overflow-x-hidden"
+          : "overflow-auto h-screen flex-col bg-gradient-to-b from-amber-100 to-white sm:overflow-x-hidden "
       }
-    >
+    > <div>
       <NavBar />
+      </div>
+      <div className="flex flex-col grow">
       <h1
         className={
-          dark ? "text-white text-4xl port0 my-4" : "text-4xl port0 my-4"
+          dark ? "text-white text-4xl port0 text-center mb-4" : "text-4xl port0 text-center mb-4"
         }
       >
         Port0
       </h1>
+      <div className="flex mx-4">
       <section
         className={
           dark
-            ? "rounded-lg p-5 text-white border md:max-w-3xl md:mt-9 border-white backdrop-blur-3xl transition-all duration-300 ease-out "
-            : "rounded-lg p-5 border md:max-w-3xl md:mt-9 border-red-900 backdrop-blur-3xl transition-all duration-300 ease-out "
+            ? "p-12 rounded-lg text-white border md:max-w-3xl md:mt-9 border-white backdrop-blur-3xl transition-all duration-300 ease-out flex flex-col justify-center w-screen ml-auto mr-auto"
+            : "p-12 rounded-lg border md:max-w-3xl md:mt-9 border-red-900 backdrop-blur-3xl transition-all duration-300 ease-out flex flex-col justify-center w-screen ml-auto mr-auto"
         }
       >
-        <h3 className="block mb-3">
+        <h3 className="block mb-3 text-2xl">
           <span className="port0">Port0</span>, the authentication service that
           is at the centre of IIITK.in.
         </h3>
         <h2 className="text-2xl">Safe.</h2>
+        <span>
         <span className="port0">Port0</span> doesn&apos;t requre your LMS
         password. It stores all your personal info in a 256-bit AES encrypted
         vault.
+        </span>
         <h2 className="text-2xl mt-3">Local.</h2>
         Whenver you login to any service, your vault is fetched from the server
         and decrypted locally on your device. Noone can access your data without
@@ -52,15 +57,16 @@ export default function Page() {
           href="/register"
           className={
             dark
-              ? "text-white bg-slate-800 border-gray-600 p-2 rounded-lg flex justify-center items-center w-30"
-              : "text-maroon border border-red-900 p-2 rounded-lg flex justify-center items-center w-30"
+              ? "text-white bg-slate-800 border-gray-600 p-2 rounded-lg flex justify-center items-center w-30 mt-4"
+              : "text-maroon border border-red-900 p-2 rounded-lg flex justify-center items-center w-30 mt-4"
           }
         >
           Register
         </Link>
       </section>
+      </div>
       <div className="grow"></div>
-      <footer>
+      <footer className="flex justify-center mt-4 grow">
         <p className={dark ? "text-white text-center" : "text-center"}>
           &copy; 2024 <span className="port0">Port0</span> Team, IIITK.in
           <br />
@@ -73,6 +79,7 @@ export default function Page() {
           </a>
         </p>
       </footer>
-    </main>
+      </div>
+    </section>
   );
 }
